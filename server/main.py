b0f50c6c6f3a325ab577d3dd5cb1c5f0eaa54974
tarinagarwal <theultimatemania64@gmail.com>
2025-12-10 02:22:26 +0530

    fix cors

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://lazydev-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
